734c3b3fe3494d96519a9993dd89398c196daf07
Teck1 <Teck1@users.noreply.github.com>
2018-07-17 00:01:21 -0700

    Finish functions in bookapp.py and fix tests.py errors

--- bookapp.py
+++ bookapp.py
@@ -1,4 +1,5 @@
 import re
+import traceback
 
 from bookdb import BookDB
 
@@ -6,18 +7,65 @@ DB = BookDB()
 
 
 def book(book_id):
-    return "<h1>a book with id %s</h1>" % book_id
+
+    body = "<p>"
+    book = DB.title_info(book_id)
+    if book is None:
+        raise NameError
+    for field, data in book.items():
+        body += "<b>{}:</b> {}<br>".format(field, data)
+    body += "</p>"
+    return body
 
 
 def books():
-    return "<h1>a list of books</h1>"
+    body = '<h1>My bookshelf:</h1>\r\n<ul>'
 
+    for book in DB.titles():
+        body += '<li><a href="/book/' + book['id'] + '">' + book['title'] + '</a></li>\r\n'
+    body += '</ul>'
+    return body
+
+def resolve_path(path):
+    funcs = {
+        '': books,
+        'book': book,
+    }
+
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
+
+    return func, args
 
 def application(environ, start_response):
     status = "200 OK"
     headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    return ["<h1>No Progress Yet</h1>".encode('utf8')]
+
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf-8')]
 
 
 if __name__ == '__main__':
